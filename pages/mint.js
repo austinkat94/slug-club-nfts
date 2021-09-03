2b2dfbef6c2c0a5f2e7d0c657d265623934e3636
austinkat94 <67551277+austinkat94@users.noreply.github.com>
2021-09-03 17:13:02 -0400

    Update mint.js

--- pages/mint.js
+++ pages/mint.js
@@ -133,7 +133,7 @@ bananas out known to man." key="twdesc" />
       <div >
           <div className="flex items-center justify-between w-full border-b-2	pb-6">
             <a href="/" className=""><img src="images/Hola.gif" width="108" alt="" className="logo-image" /></a>
-            <nav className="flex flex-wrap flex-row justify-around Poppitandfinchsans">
+            <nav className="flex flex-wrap flex-row justify-around ">
               <a href="/#about" className="text-4xl text-white hover:text-black m-6">About</a>
               <a href="/mint" className="text-4xl text-white hover:text-black m-6">Mint!</a>
               <a href="/#team" className="text-4xl text-white hover:text-black m-6">Team</a>
@@ -143,9 +143,9 @@ bananas out known to man." key="twdesc" />
              
           </div>
           <div className="flex auth my-8 font-bold  justify-center items-center vw2">
-            {!signedIn ? <button onClick={signIn} className="montserrat inline-block border-2 border-black bg-white border-opacity-100 no-underline hover:text-black py-2 px-4 mx-4 shadow-lg hover:bg-blue-500 hover:text-gray-100">Connect Wallet with Metamask</button>
+            {!signedIn ? <button onClick={signIn} className=" inline-block border-2 border-black bg-white border-opacity-100 no-underline hover:text-black py-2 px-4 mx-4 shadow-lg hover:bg-blue-500 hover:text-gray-100">Connect Wallet with Metamask</button>
             :
-            <button onClick={signOut} className="montserrat inline-block border-2 border-black bg-white border-opacity-100 no-underline hover:text-black py-2 px-4 mx-4 shadow-lg hover:bg-blue-500 hover:text-gray-100">Wallet Connected: {walletAddress}</button>}
+            <button onClick={signOut} className=" inline-block border-2 border-black bg-white border-opacity-100 no-underline hover:text-black py-2 px-4 mx-4 shadow-lg hover:bg-blue-500 hover:text-gray-100">Wallet Connected: {walletAddress}</button>}
           </div>
         </div>
 
@@ -156,10 +156,10 @@ bananas out known to man." key="twdesc" />
 
             <div className="flex flex-col items-center">
 
-                <span className="flex Poppitandfinchsans text-5xl text-white items-center bg-grey-lighter rounded rounded-r-none my-4 ">TOTAL SLUGGIES MINTED:  <span className="text-blau text-6xl"> {!signedIn ?  <>-</>  :  <>{totalSupply}</> } / 5500</span></span>
+                <span className="flex  text-5xl text-white items-center bg-grey-lighter rounded rounded-r-none my-4 ">TOTAL SLUGGIES MINTED:  <span className="text-blau text-6xl"> {!signedIn ?  <>-</>  :  <>{totalSupply}</> } / 5500</span></span>
 
                 <div id="mint" className="flex justify-around  mt-8 mx-6">
-                  <span className="flex Poppitandfinchsans text-5xl text-white items-center bg-grey-lighter rounded rounded-r-none px-3 font-bold">GIMME</span>
+                  <span className="flex  text-5xl text-white items-center bg-grey-lighter rounded rounded-r-none px-3 font-bold">GIMME</span>
                   
                   <input 
                                       type="number" 
@@ -168,15 +168,15 @@ bananas out known to man." key="twdesc" />
                                       value={how_many_slugs}
                                       onChange={ e => set_how_many_slugs(e.target.value) }
                                       name="" 
-                                      className="Poppitandfinchsans pl-4 text-4xl  inline bg-grey-lighter  py-2 font-normal rounded text-grey-darkest  font-bold"
+                                      className=" pl-4 text-4xl  inline bg-grey-lighter  py-2 font-normal rounded text-grey-darkest  font-bold"
                                   />
                   
-                  <span className="flex Poppitandfinchsans text-5xl text-white items-center bg-grey-lighter rounded rounded-r-none px-3 font-bold">SLUGGIES!</span>
+                  <span className="flex  text-5xl text-white items-center bg-grey-lighter rounded rounded-r-none px-3 font-bold">SLUGGIES!</span>
     
                 </div>
                 {saleStarted ? 
-                <button onClick={() => mintSluggies(how_many_slugs)} className="mt-4 Poppitandfinchsans text-4xl border-6 bg-blau  text-white hover:text-black p-2 ">MINT {how_many_slugs} sluggies for {(slugPrice * how_many_slugs) / (10 ** 18)} ETH + GAS</button>        
-                  : <button className="mt-4 Poppitandfinchsans text-4xl border-6 bg-blau  text-white hover:text-black p-2 ">SALE IS NOT ACTIVE OR NO WALLET IS CONNECTED</button>        
+                <button onClick={() => mintSluggies(how_many_slugs)} className="mt-4  text-4xl border-6 bg-blau  text-white hover:text-black p-2 ">MINT {how_many_slugs} sluggies for {(slugPrice * how_many_slugs) / (10 ** 18)} ETH + GAS</button>        
+                  : <button className="mt-4  text-4xl border-6 bg-blau  text-white hover:text-black p-2 ">SALE IS NOT ACTIVE OR NO WALLET IS CONNECTED</button>        
             
               }
                 

abab842f37bf8d2496c6bf44b3a1cece2c112462
austinkat94 <67551277+austinkat94@users.noreply.github.com>
2021-09-18 20:27:58 -0400

    Update [id].js

--- pages/api/[id].js
+++ pages/api/[id].js
@@ -42,6 +42,10 @@ const slugApi = async(req, res) => {
     
     const signatures = [137,883,1327,1781,2528,2763,3833,5568,5858,6585,6812,7154,8412]
     const trait = traits[parseInt(query)]
+    const options_1 = ['Top Prop', 'Middle Prop', 'Bottom Prop']
+    const options_2 = ['Top Prop', 'Middle Prop']
+    const options_3 = ['Top Prop', 'Bottom Prop']
+    const options_4 = ['Middle Prop', 'Bottom Prop']
     // const trait = traits[ Math.floor(Math.random() * 8888) ] // for testing on rinkeby 
 
     // CHECK OPENSEA METADATA STANDARD DOCUMENTATION https://docs.opensea.io/docs/metadata-standards
@@ -63,8 +67,8 @@ const slugApi = async(req, res) => {
         ]
       }
       // console.log(metadata)
-    } else {
-    // GENERAL BANANA METADATA
+    } else if (options_1.every(key => Object.keys(trait).includes(key))){
+    // GENERAL Slug METADATA
       metadata = {
         "name": tokenName,
         "description": "The Slug Club is a brother and sister created endeavor to release slugs into the crypto space. These slugs all carry various traits and some are one of a kind!",
@@ -107,12 +111,252 @@ const slugApi = async(req, res) => {
             {
               "trait_type": "Bottom Prop",
               "value": trait["Bottom Prop"]
-            },
+            }
         ]
       }
       
       // console.log(metadata)
 
+    } else if (options_2.every(key => Object.keys(trait).includes(key))) {
+         metadata = {
+        "name": tokenName,
+        "description": "The Slug Club is a brother and sister created endeavor to release slugs into the crypto space. These slugs all carry various traits and some are one of a kind!",
+        "tokenId" : parseInt(query),
+        "image": `https://ipfs.io/ipfs/${trait["imageIPFS"]}`,
+        "external_url":"https://www.boringbananas.co",
+        "attributes": [          
+            {
+              "trait_type": "Background",
+              "value": trait["Background"]
+            },
+            {
+              "trait_type": "Stomach",
+              "value": trait["Stomach"]
+            },
+            {
+              "trait_type": "Back",
+              "value": trait["Back"]
+            },
+            {
+              "trait_type": "Eyes",
+              "value": trait["Eyes"]
+            },
+            {
+              "trait_type": "Mouth",
+              "value": trait["Mouth"]
+            },
+            {
+              "trait_type": "Shell",
+              "value": trait["Shell"]
+            },
+            {
+              "trait_type": "Top Prop",
+              "value": trait["Top Prop"]
+            },
+            {
+              "trait_type": "Middle Prop",
+              "value": trait["Middle Prop"]
+            }
+        ]
+      }
+    } else if (options_3.every(key => Object.keys(trait).includes(key))) {
+         metadata = {
+        "name": tokenName,
+        "description": "The Slug Club is a brother and sister created endeavor to release slugs into the crypto space. These slugs all carry various traits and some are one of a kind!",
+        "tokenId" : parseInt(query),
+        "image": `https://ipfs.io/ipfs/${trait["imageIPFS"]}`,
+        "external_url":"https://www.boringbananas.co",
+        "attributes": [          
+            {
+              "trait_type": "Background",
+              "value": trait["Background"]
+            },
+            {
+              "trait_type": "Stomach",
+              "value": trait["Stomach"]
+            },
+            {
+              "trait_type": "Back",
+              "value": trait["Back"]
+            },
+            {
+              "trait_type": "Eyes",
+              "value": trait["Eyes"]
+            },
+            {
+              "trait_type": "Mouth",
+              "value": trait["Mouth"]
+            },
+            {
+              "trait_type": "Shell",
+              "value": trait["Shell"]
+            },
+            {
+              "trait_type": "Top Prop",
+              "value": trait["Top Prop"]
+            },
+            {
+              "trait_type": "Bottom Prop",
+              "value": trait["Bottom Prop"]
+            }
+        ]
+      }
+    } else if (options_4.every(key => Object.keys(trait).includes(key))) {
+         metadata = {
+        "name": tokenName,
+        "description": "The Slug Club is a brother and sister created endeavor to release slugs into the crypto space. These slugs all carry various traits and some are one of a kind!",
+        "tokenId" : parseInt(query),
+        "image": `https://ipfs.io/ipfs/${trait["imageIPFS"]}`,
+        "external_url":"https://www.boringbananas.co",
+        "attributes": [          
+            {
+              "trait_type": "Background",
+              "value": trait["Background"]
+            },
+            {
+              "trait_type": "Stomach",
+              "value": trait["Stomach"]
+            },
+            {
+              "trait_type": "Back",
+              "value": trait["Back"]
+            },
+            {
+              "trait_type": "Eyes",
+              "value": trait["Eyes"]
+            },
+            {
+              "trait_type": "Mouth",
+              "value": trait["Mouth"]
+            },
+            {
+              "trait_type": "Shell",
+              "value": trait["Shell"]
+            },
+            {
+              "trait_type": "Middle Prop",
+              "value": trait["Middle Prop"]
+            },
+            {
+              "trait_type": "Bottom Prop",
+              "value": trait["Bottom Prop"]
+            }
+        ]
+      }
+    } else if ("Top Prop" in trait) {
+         metadata = {
+        "name": tokenName,
+        "description": "The Slug Club is a brother and sister created endeavor to release slugs into the crypto space. These slugs all carry various traits and some are one of a kind!",
+        "tokenId" : parseInt(query),
+        "image": `https://ipfs.io/ipfs/${trait["imageIPFS"]}`,
+        "external_url":"https://www.boringbananas.co",
+        "attributes": [          
+            {
+              "trait_type": "Background",
+              "value": trait["Background"]
+            },
+            {
+              "trait_type": "Stomach",
+              "value": trait["Stomach"]
+            },
+            {
+              "trait_type": "Back",
+              "value": trait["Back"]
+            },
+            {
+              "trait_type": "Eyes",
+              "value": trait["Eyes"]
+            },
+            {
+              "trait_type": "Mouth",
+              "value": trait["Mouth"]
+            },
+            {
+              "trait_type": "Shell",
+              "value": trait["Shell"]
+            },
+            {
+              "trait_type": "Top Prop",
+              "value": trait["Top Prop"]
+            }
+        ]
+      }
+    } else if ("Middle Prop" in trait) {
+         metadata = {
+        "name": tokenName,
+        "description": "The Slug Club is a brother and sister created endeavor to release slugs into the crypto space. These slugs all carry various traits and some are one of a kind!",
+        "tokenId" : parseInt(query),
+        "image": `https://ipfs.io/ipfs/${trait["imageIPFS"]}`,
+        "external_url":"https://www.boringbananas.co",
+        "attributes": [          
+            {
+              "trait_type": "Background",
+              "value": trait["Background"]
+            },
+            {
+              "trait_type": "Stomach",
+              "value": trait["Stomach"]
+            },
+            {
+              "trait_type": "Back",
+              "value": trait["Back"]
+            },
+            {
+              "trait_type": "Eyes",
+              "value": trait["Eyes"]
+            },
+            {
+              "trait_type": "Mouth",
+              "value": trait["Mouth"]
+            },
+            {
+              "trait_type": "Shell",
+              "value": trait["Shell"]
+            },
+            {
+              "trait_type": "Middle Prop",
+              "value": trait["Middle Prop"]
+            }
+        ]
+      }
+    } else if ("Bottom Prop" in trait) {
+         metadata = {
+        "name": tokenName,
+        "description": "The Slug Club is a brother and sister created endeavor to release slugs into the crypto space. These slugs all carry various traits and some are one of a kind!",
+        "tokenId" : parseInt(query),
+        "image": `https://ipfs.io/ipfs/${trait["imageIPFS"]}`,
+        "external_url":"https://www.boringbananas.co",
+        "attributes": [          
+            {
+              "trait_type": "Background",
+              "value": trait["Background"]
+            },
+            {
+              "trait_type": "Stomach",
+              "value": trait["Stomach"]
+            },
+            {
+              "trait_type": "Back",
+              "value": trait["Back"]
+            },
+            {
+              "trait_type": "Eyes",
+              "value": trait["Eyes"]
+            },
+            {
+              "trait_type": "Mouth",
+              "value": trait["Mouth"]
+            },
+            {
+              "trait_type": "Shell",
+              "value": trait["Shell"]
+            },
+            {
+              "trait_type": "Bottom Prop",
+              "value": trait["Bottom Prop"]
+            }
+        ]
+      }   
     }
     
     res.statusCode = 200

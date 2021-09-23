a642e552b6baa3a3ee3fac7161b50e1915f74392
austinkat94 <67551277+austinkat94@users.noreply.github.com>
2021-09-22 22:07:48 -0400

    Update app.js

--- pages/app.js
+++ pages/app.js
@@ -1,10 +1,16 @@
-$.ajax({
-    url: 'https://ipfs.infura.io:5001/api/v0/cat?arg=QmWfY26GE7exeyfVm83Ep6jpcHSqC9utb2Sj3UUUQYT3px',
-    beforeSend: function(xhr) {
-         xhr.setRequestHeader("Authorization", "1yMVD6UK7R33PIatmGgyVVIqHVB:55f79f348c407b124c4de1e4a0938e7a")
-    }, success: function(data){
-        alert(data);
-        //process the JSON data etc
-        console.log(data)
+const IPFS = require('ipfs')
+
+const main = async(req, res) => {
+    const node = await IPFS.create()
+
+    const stream = node.cat('QmWfY26GE7exeyfVm83Ep6jpcHSqC9utb2Sj3UUUQYT3px')
+    let data = ''
+
+    for await (const chunk of stream) {
+    // chunks of data are returned as a Buffer, convert it back to a string
+    data += chunk.toString()
     }
-})
+
+    console.log(data)
+}
+export default main

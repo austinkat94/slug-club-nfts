d646b63d986043c4415503738173f56adac20d72
austinkat94 <67551277+austinkat94@users.noreply.github.com>
2021-09-22 21:07:51 -0400

    Create app.js

---
+++ pages/api/app.js
@@ -0,0 +1,15 @@
+const IPFS = require("ipfs")
+
+async function main () {
+
+const node = await IPFS.create()
+
+const cid = "QmWfY26GE7exeyfVm83Ep6jpcHSqC9utb2Sj3UUUQYT3px"
+
+const response = await ipfs.get(cid)
+
+console.log(response.text)
+
+}
+
+main()

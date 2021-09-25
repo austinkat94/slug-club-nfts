0b3fce601664a9594e6231f7b430b69ded369155
austinkat94 <67551277+austinkat94@users.noreply.github.com>
2021-09-25 08:43:08 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -45,7 +45,7 @@ export default function Home() {
             <div className="flex flex-wrap lg:flex-nowrap justify-around items-center">
                   <div className="lg:w-1/2 w-3/4">
                     <h1 className="text-7xl text-black amatic">Everyone, meet <br/><span className="text-green-700">sluggies</span></h1>
-                    <p className="text-2xl text-black my-6">There are 40,000 species of gastropods on earth, but here in The Slug Club we have 10,000 exotic Sluggies for you to collect! Our 3 person team has personally cared for these creatures and are looking forward to them going to good homes!
+                    <p className="text-2xl text-black my-6">There are 40,000 species of gastropods on earth, but here in The Slug Club we have 10,000 exotic Sluggies for you to collect! <br /> Our 3 person team has personally cared for these creatures and are looking forward to them going to good homes!
                     </p>
                     <p className="text-2xl text-black my-6">
                     <strong>LAUNCH DATE:</strong> COMING SOON! <br />
